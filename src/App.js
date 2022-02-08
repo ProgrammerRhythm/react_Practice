5ab800e63e2c2c46eec7704ad31fc959e537eb62
Rhythm Munshi <macmini@Macs-Mac-mini.local>
2022-02-08 15:51:55 +0600

    Going to be a smart React Developer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>My name is Rhtyhm</p>
+       <Call></Call>
+      <Product name="Photoshop" price="$99.99"></Product>
+      <Product name="Photoshop" price="$99.99"></Product>
+      <Product name="Photoshop" price="$99.99"></Product>
+      <Product name="Photoshop" price="$99.99"></Product>
+      <Product name="Photoshop" price="$99.99"></Product>
+      <Product name="Photoshop" price="$99.99"></Product>
       </header>
     </div>
   );
 }
+function Product(props){
+  const style = {
+    border:'2px solid gray',
+    color:'white',
+    backgroundColor:"lightGray",
+    padding:'50px',
+    marginBottom:'20px',
+  }
+  return(
+    <div style={style}>
+      <h2>{props.name}</h2>
+      <h1>{props.price}</h1>
+      <button style={{padding:'10px' ,backgroundColor:'blue',color:'white',border:'none',fontSize:'19px'}}>Buy now</button>
+    </div>
+  )
+}
 
+
+
+function Call(){
+  return (
+    <div>
+        <h1 style={{border: '2px solid white', padding: "15px",borderRadius:'15px'}}>Name : Rhythm</h1>
+        <h2 style={{color:"lightBlue"}}>Age : 15</h2>
+    </div>
+  )
+}
 export default App;
